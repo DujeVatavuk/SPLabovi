79ad2da5ece0274ffeee41786c15bf18076e0165
Duje Vatavuk <duje@vatavuk.com>
2023-12-17 23:09:23 +0100

    added task9 stuff to Lab8 project, except writing in file

--- Lab8/Lab8.c
+++ Lab8/Lab8.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <time.h>
 
 // Struktura èvora binarnog stabla
 struct _node;
@@ -20,22 +21,48 @@ int postorder(Position root);
 int levelOrder(Position root);
 Position search(Position root, int value);
 Position deleteNode(Position root, int value);
+int replace(Position root);
+int random();
 
 int main() {
-    /*Node head = {
-        .value = 0,
-        .left = NULL,
-        .right = NULL,
-    };*/
-
     Position root = NULL;
+    Position rootRand = NULL;
+    srand(time(0));
 
-    int rootValue = 0;
+    /*int rootValue = 0;
     printf("\033[0;32mEnter root value: \033[0m");
     scanf("%d", &rootValue);
-    root = insert(root, rootValue);
+    root = insert(root, rootValue);*/
 
-    while (1) {
+    root = insert(root, 2);
+    insert(root, 5);
+    insert(root, 7);
+    insert(root, 8);
+    insert(root, 11);
+    insert(root, 1);
+    insert(root, 4);
+    insert(root, 2);
+    insert(root, 3);
+    insert(root, 7);
+
+    levelOrder(root);
+
+    replace(root);
+
+    printf("\n");
+    levelOrder(root);
+
+    rootRand = insert(rootRand, random());
+    for (int i = 0; i < 10; i++) {
+        insert(rootRand, random());
+    }
+
+    printf("\n");
+    levelOrder(rootRand);
+
+    //rand()
+
+    /*while (1) {
         printf("\nMenu:\n");
         printf("1 - Insert node\n");
         printf("2 - Search node\n");
@@ -100,7 +127,7 @@ int main() {
         else {
             printf("\033[0;31mInvalid choice. Please enter a valid option.\033[0m\n");
         }
-    }
+    }*/
 
     return EXIT_SUCCESS;
 }
@@ -131,7 +158,7 @@ Position insert(Position root, int value) {
 }
 
 int inorder(Position root) {
-    if (root != NULL) {
+    if (root) {
         inorder(root->left);
         printf("%d ", root->value);
         inorder(root->right);
@@ -140,7 +167,7 @@ int inorder(Position root) {
 }
 
 int preorder(Position root) {
-    if (root != NULL) {
+    if (root) {
         printf("%d ", root->value);
         preorder(root->left);
         preorder(root->right);
@@ -149,7 +176,7 @@ int preorder(Position root) {
 }
 
 int postorder(Position root) {
-    if (root != NULL) {
+    if (root) {
         postorder(root->left);
         postorder(root->right);
         printf("%d ", root->value);
@@ -161,8 +188,7 @@ int levelOrder(Position root) {
     if (root == NULL)
         return;
 
-    // Red za èekanje èvorova koji se trebaju posetiti
-    Position queue[100];
+    Position queue[100] = { 0 };
     int front = 0, rear = 0;
 
     queue[rear++] = root;
@@ -195,13 +221,13 @@ Position deleteNode(Position root, int value) {
     if (root == NULL)
         return root;
 
-    // Pronalazak èvora koji se treba obrisati
+    // Find node
     if (value < root->value)
         root->left = deleteNode(root->left, value);
     else if (value > root->value)
         root->right = deleteNode(root->right, value);
     else {
-        // Èvor sa samo jednim ili bez dece
+        // Node with one or no children
         if (root->left == NULL) {
             Position temp = root->right;
             free(root);
@@ -213,17 +239,40 @@ Position deleteNode(Position root, int value) {
             return temp;
         }
 
-        // Èvor sa dvoje dece, pronaði najmanji èvor u desnom podstablu (naslednika)
+        // Node with two children, find smallest in the right tree
         Position temp = root->right;
         while (temp->left != NULL)
             temp = temp->left;
 
-        // Kopiranje vrednosti najmanjeg èvora u trenutni èvor
+        // Copy value from smallest to current node
         root->value = temp->value;
 
-        // Brisanje najmanjeg èvora iz desno podstabla
+        // Delete smallest from the right tree
         root->right = deleteNode(root->right, temp->value);
     }
 
     return root;
+}
+
+int replace(Position root) {
+    if (root == NULL) {
+        return 0;
+    }
+
+    // Recursively update left and right subtrees first
+    int leftValue = replace(root->left);
+    int rightValue = replace(root->right);
+
+    // Store the original value
+    int originalValue = root->value;
+
+    // Update the value of the current node to be the sum of its children
+    root->value = leftValue + rightValue;
+
+    // Return the sum of the node's value and the sum of its children
+    return originalValue + root->value;
+}
+
+int random() {
+    return (rand() % (90 - 10 + 1)) + 10;
 }
